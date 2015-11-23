ef21a3d93d0b2b5b3f3906563f03c944e72d7f0f
Sahir Boghani <sahir@lin114-10.cise.ufl.edu>
2015-11-23 11:36:09 -0500

    Basic customer menu

--- Project.java
+++ Project.java
@@ -5,70 +5,84 @@ import java.util.*;
 public class Project {
 	
 	static Scanner sc = new Scanner(System.in);
-	
+	static String user = "sahir";
+	static String pass = "abcd1234";
+
 	public static void main(String[] args) {
 		
 		Connection sqlcon  = null;
 		Statement sqlStatement  = null;
 		ResultSet myResultSet  = null;
-		String user = "sahir";
-		String pass = "abcd1234";
 		
 		try { 
 			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
 			
 			// Connect to the database
-			sqlcon= DriverManager.getConnection ("jdbc:oracle:thin:hr/hr@oracle.cise.ufl.edu:1521:orcl", user, pass);
+			sqlcon= DriverManager.getConnection("jdbc:oracle:thin:hr/hr@oracle.cise.ufl.edu:1521:orcl", user, pass);
 			
 			
 			System.out.println("\nConnected to database...\n");
 			
 			while(true) {
+
 				// Create a Statement
 				sqlStatement = sqlcon.createStatement();
 			
 				String q = "";
 			
-				int choice = isCustomer();
+				int user = getUser();
 				
-				if(choice == 3)
+				if(user == 3)
 					break;
-			
-				boolean customer = choice == 1;
+				
+				if(user == 1) { // user is a customer
+					
+					int choice = getCustomerChoice();
+					
+					switch(choice) {
 
-				if(customer) {
-					choice = 1;// getCustomerChoice();
-					if(choice == 1) {
-						System.out.print("Search: ");
-						String search = sc.next();
-						System.out.println();
-						q = "select * from DB_Product where name like \'%" +search+ "%\' order by price";
-						myResultSet = sqlStatement.executeQuery(q);
+						case 1:						
+								System.out.print("Search: ");
+								String search = sc.next();
+								System.out.println();
+								q = "select * from DB_Product where name like \'%" + search + "%\' order by price";
+								myResultSet = sqlStatement.executeQuery(q);
+								
+								while(myResultSet.next()) {
+									System.out.println("\n-------------------------------------------------------");
+									System.out.println("ID:            \t\t\t" + myResultSet.getObject(1).toString());
+									System.out.println("Name:          \t\t\t" + myResultSet.getObject(2).toString());
+									System.out.println("Description:   \t\t\t" + myResultSet.getObject(3).toString());
+									System.out.println("Stock Quantity:\t\t\t" + myResultSet.getObject(4).toString());
+									System.out.println("Price:         \t\t\t" + myResultSet.getObject(5).toString());
+									System.out.println("Active:        \t\t\t" + myResultSet.getObject(6).toString());					 
+								}
+								break;
+				
+						case 2:
+
+								break;
 						
-						while(myResultSet.next()) {
-							System.out.println("\n-------------------------------------------------------");
-							System.out.println("ID:            \t\t\t" + myResultSet.getObject(1).toString());
-							System.out.println("Name:          \t\t\t" + myResultSet.getObject(2).toString());
-							System.out.println("Description:   \t\t\t" + myResultSet.getObject(3).toString());
-							System.out.println("Stock Quantity:\t\t\t" + myResultSet.getObject(4).toString());
-							System.out.println("Price:         \t\t\t" + myResultSet.getObject(5).toString());
-							System.out.println("Active:        \t\t\t" + myResultSet.getObject(6).toString());					 
-						}
+						case 3:
 					}
+				}
+				else { // user is a staff member
+
 				}
 			}
 			
 			sqlStatement.close();
-			
 			sqlcon.close();
-		} catch (SQLException ex) {
-	
-			System.out.println("SQLException:" + ex.getMessage() + "<BR>");
-		}
+
+		} catch(SQLException e) {
+			System.out.println("SQLException:" + e.getMessage() + " <BR>");
+		} catch(Exception e) {
+			System.out.println("Exception: " + e.getMessage() + " <BR>");
+		} 
 	} 
 	
-	public static int isCustomer() {
-		System.out.println("\nMake selection");
+	public static int getUser() {
+		System.out.println("\nMake Selection");
 		System.out.println("1. Customer");
 		System.out.println("2. Staff");
 		System.out.println("3. Exit");
@@ -76,10 +90,26 @@ public class Project {
 		int choice = sc.nextInt();
 		
 		if(choice < 1 || choice > 3) {
-			System.out.println("Incorrect choice, try again");
-			return isCustomer();
+			System.out.println("\nIncorrect choice, try again\n");
+			return getUser();
 		}
 		
+		return choice;
+	}
+
+	public static int getCustomerChoice() {
+		System.out.println("\nMake Selection");
+		System.out.println("1. Basic Search");
+		System.out.println("2. Create/Update/Delete Account");
+		System.out.println("3. Sign In");
+
+		int choice = sc.nextInt();
+
+		if(choice < 1 || choice > 3) {
+			System.out.println("\nIncorrect choice, try again\n");
+			return getCustomerChoice();
+		}
+
 		return choice;
 	}
 }
